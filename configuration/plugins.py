6598633c8dc7f2b19c5ff97a7a2120a35ff30fe9
Maximilian Falkenstein <maxf@njsm.de>
2023-10-07 14:05:03 +0200

    Read secret from file

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -1,6 +1,14 @@
 # Add your plugins and plugin settings here.
 # Of course uncomment this file out.
 
+password = "dummyvalue"
+try:
+    with open("/run/secrets/netbox/napalm_password", "r") as file:
+        password = file.read().strip()
+except Exception:
+    pass
+
+
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
@@ -13,8 +21,8 @@ PLUGINS = ["netbox_napalm_plugin"]
 # }
 PLUGINS_CONFIG = {
     'netbox_napalm_plugin': {
-        'NAPALM_USERNAME': 'xxx',
-        'NAPALM_PASSWORD': 'yyy',
+        'NAPALM_USERNAME': 'napalmviewer',
+        'NAPALM_PASSWORD': password,
     },
 }
 
