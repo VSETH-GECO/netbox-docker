bcc06306bfbe01520dbfdc035bf8d2a9ceeb2a19
Maximilian Falkenstein <maxf@njsm.de>
2023-10-07 13:29:27 +0200

    Add netbox-napalm plugin on top of 2.7.0

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -4,10 +4,17 @@
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
-# PLUGINS = ["netbox_bgp"]
+PLUGINS = ["netbox_napalm_plugin"]
 
 # PLUGINS_CONFIG = {
 #   "netbox_bgp": {
 #     ADD YOUR SETTINGS HERE
 #   }
 # }
+PLUGINS_CONFIG = {
+    'netbox_napalm_plugin': {
+        'NAPALM_USERNAME': 'xxx',
+        'NAPALM_PASSWORD': 'yyy',
+    },
+}
+
