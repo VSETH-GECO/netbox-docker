2191e78a38a90e36b2fadd0e2fe94ade5c5b639f
Florian Bütler <florian.buetler@proton.me>
2025-10-18 22:48:59 +0200

    maybe fix plugin discovery

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -1,13 +1,23 @@
 # Add your plugins and plugin settings here.
 # Of course uncomment this file out.
 
+from os import environ
+
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
-# PLUGINS = ["netbox_bgp"]
+# # Enable installed plugins. Add the name of each plugin to the list.
+PLUGINS = [
+    'netbox_napalm_plugin'
+]
 
-# PLUGINS_CONFIG = {
-#   "netbox_bgp": {
-#     ADD YOUR SETTINGS HERE
-#   }
-# }
+# # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
+# # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
+
+
+PLUGINS_CONFIG = {
+    'netbox_napalm_plugin': {
+        'NAPALM_USERNAME': 'napalmviewer',
+        'NAPALM_PASSWORD': environ.get('NAPALM_PASSWORD', "dummyvalue"),
+    },
+}
