ebb53290cf7c86b4d8bad194850df244001528db
Florian Bütler <florian.buetler@proton.me>
2025-10-18 23:16:06 +0200

    install netbox_qrcode plugin

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -8,7 +8,8 @@ from os import environ
 
 # # Enable installed plugins. Add the name of each plugin to the list.
 PLUGINS = [
-    'netbox_napalm_plugin'
+    'netbox_napalm_plugin',
+    'netbox_qrcode'
 ]
 
 # # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
